20dbe6d62db155464c40294fb71a37be5175c486
yorek <info@davidemauri.it>
2016-02-25 12:50:19 +0100

    Added setup-view-1.sql file

---
+++ support/sample/setup/setup-views-1.sql
@@ -0,0 +1,16 @@
+﻿/*
+#
+# Adaptive BI Framework 3.0
+#
+# Last Revision: 2
+# Last Date: 2016-02-25
+# Last Author: dmauri
+#
+# Setup object to make sure samples runs correcly
+#
+# NOTE: Make sure SQLCMD mode is eneabled
+#
+*/
+
+:r ddl\setup-cfg-views.sql
+:r ddl\setup-hlp-views.sql
